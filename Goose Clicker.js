721897d56431e4ef7df91bf05026a8394baa0f29
Hy Lac <121268118+kingMonkeh@users.noreply.github.com>
2024-10-12 16:45:03 -0400

    Save Files? (TEST)

--- Goose Clicker.js
+++ Goose Clicker.js
@@ -1,9 +1,18 @@
 var gooses = 0;
 var gooseThisSecond = 0; //number of geese produced this second
 
-const owned = [0, 0, 0];
-const price = [10, 1000000, 100];
-const produce = [1, 12345, 10];
+//items = [[owned, price, produce], ...]
+const items = [[0, 10, 1], [0, 100, 10], [0, 1000000, 12345]];
+
+function init(){
+    for (let i = 0; i < items.length; i++){
+        for (let j = 0; j < 3; j++){
+            if (localStorage.getItem(i + "_" + j) != null){
+                items[i][j] = localStorage.getItem(i + "_" + j);
+            }
+        }
+    }
+}
 
 const mainGoose = document.getElementById("mainGoose");
 const gooseCount = document.getElementById("gooseCount");
@@ -45,34 +54,34 @@ mainGoose.addEventListener('click', function(){
 //Buy grass
 grass.addEventListener('click', function(){
     //Check if you can buy
-    if (gooses >= price[0]){
-        owned[0]++;
-        gooses -= price[0];
+    if (gooses >= items[0][1]){
+        items[0][0]++;
+        gooses -= items[0][1];
     }
     //Update display
-    grassRightTop.innerHTML = "Price: " + price[0];
-    grassRightBottom.innerHTML = "Owned: " + owned[0];
+    grassRightTop.innerHTML = "Price: " + items[0][1];
+    grassRightBottom.innerHTML = "Owned: " + items[0][0];
 });
 
 //Buy wheat
 wheat.addEventListener('click', function(){
-    if (gooses >= price[2]){
-        owned[2]++;
-        gooses -= price[2];
+    if (gooses >= items[1][1]){
+        items[1][0]++;
+        gooses -= items[1][1];
     }
-    wheatRightTop.innerHTML = "Price: " + price[2];
-    wheatRightBottom.innerHTML = "Owned: " + owned[2];
+    wheatRightTop.innerHTML = "Price: " + items[1][1];
+    wheatRightBottom.innerHTML = "Owned: " + items[1][0];
 });
 
 //Buy uwaterloo campus
 uwaterloo.addEventListener('click', function(){
-    if (gooses >= price[1]){
-        owned[1]++;
-        gooses -= price[1];
+    if (gooses >= items[2][1]){
+        items[2][0]++;
+        gooses -= items[2][1];
     }
     //update display
-    uwaterlooRightTop.innerHTML = "Price: " + price[1];
-    uwaterlooRightBottom.innerHTML = "Owned: " + owned[1];
+    uwaterlooRightTop.innerHTML = "Price: " + items[2][1];
+    uwaterlooRightBottom.innerHTML = "Owned: " + items[2][0];
 });
 
 //This interval does 2 things:
@@ -80,9 +89,9 @@ uwaterloo.addEventListener('click', function(){
 //Count geese per second
 setInterval(function(){
 
-    for (let i = 0; i < owned.length; i++){
-        gooses += owned[i] * produce[i];
-        gooseThisSecond += owned[i] * produce[i];
+    for (let i = 0; i < items.length; i++){
+        gooses += items[i][0] * items[i][2];
+        gooseThisSecond += items[i][0] * items[i][2];
     }
 
     goosePerSecond.innerHTML = "Geese per second: " + gooseThisSecond;
@@ -91,3 +100,15 @@ setInterval(function(){
     gooseCount.innerHTML = gooses + " Geese";
     
 }, 1000);
+
+setInterval(function(){
+
+    for (let i = 0; i < items.length; i++){
+        for (let j = 0; j < 3; j++){
+            localStorage.setItem(i + "_" + j, items[i][j]);
+        }
+    }
+
+    console.log("Saved!");
+
+}, 30000)
