73c64f272761d6d1653ae36aadab53eac7f38642
Hy Lac <121268118+kingMonkeh@users.noreply.github.com>
2024-11-24 13:24:32 -0500

    UI Upgrade
    -Finished items
    -Better UI theme
    -Start of artifacts

--- Goose Clicker.js
+++ Goose Clicker.js
@@ -1,15 +1,38 @@
+/*
+
+TO DO
+
+1. Add RESET Error Box (COMPLETE)
+2. Round Geese (COMPLETE)
+5/2. News Station (COMPLETE)
+3. Add Artifacts
+4. Make Game Look Better
+5. Stats
+6. Plinko
+7. Add goose click luckmaxx
+8. Fries in the bag
+9. Add commas to the numbers
+...
+inf. Learn React
+
+*/
+
 var gooses = 0;
 var multiplier = 1;
 var gooseThisSecond = 0; //number of geese produced this second
 
 //items = [[owned, price, produce], ...]
-const items = [[0, 10, 1], [0, 100, 10], [0, 1019, 111], [0, 10000, 1010], [0, 1000000, 12345], [0, 10000000, 123456], [0, 100000000, 1234567], [0, 1000000000, 12345678], [0, 10000000000, 12345678]];
+var items = [[0, 10, 1], [0, 100, 10], [0, 1019, 111], [0, 10000, 1010], [0, 1000000, 12345], [0, 10000000, 123456], [0, 100000000, 1234567], [0, 1000000000, 12345678], [0, 10000000000, 123456789]];
+const origItems = [[0, 10, 1], [0, 100, 10], [0, 1019, 111], [0, 10000, 1010], [0, 1000000, 12345], [0, 10000000, 123456], [0, 100000000, 1234567], [0, 1000000000, 12345678], [0, 10000000000, 123456789]];
+
+var artifacts = ["fertilizer", "betterSeeds", "goldenWheatStrain", "babylonianIrrigationSystem", "shinyCoating", "featherInsulation", "geeseScholarship"];
+var artifactsOwned = new Map();
 
 //helper1: string -> [mainbox html, price html, owned html]
 //let document.getElementById() = d.gEBI()
 //helper1("grass") -> [d.gEBI("grass"), d.gEBI("grassRightTop", d.gEBI("grassRightBottom"), d.gEBI("grassLeft"), d.gEBI("grassImage")]
 function helper1(item){
-    return [document.getElementById(item), document.getElementById(item + "RightTop"), document.getElementById(item + "RightBottom"), document.getElementById(item + "Left"), document.getElementById(item + "Image")];
+    return [document.getElementById(item), document.getElementById(item + "Price"), document.getElementById(item + "Owned"), document.getElementById(item + "Image")];
 }
 
 const htmlItems = [helper1("grass"), helper1("wheat"), helper1("gooseStatue"), helper1("gooseNest"), helper1("uwaterloo"), helper1("gooseTemple"), helper1("gooseMagnet"), helper1("gooseLab"), helper1("portal")];
@@ -22,6 +45,7 @@ function helper2(left, image){
 
 //Initialize game, aka local save file
 function init(){
+
     console.log("Working");
 
     //Init goose count
@@ -39,9 +63,32 @@ function init(){
             }
         }
         //Update html text
-        htmlItems[i][1].innerHTML = "Price: " + localStorage.getItem(i + "_" + 1);
-        htmlItems[i][2].innerHTML = "Owned: " + localStorage.getItem(i + "_" + 0);
+        if (localStorage.getItem(i + "_" + 1) != null){
+            htmlItems[i][1].innerHTML = "Price: " + localStorage.getItem(i + "_" + 1);
+            htmlItems[i][2].innerHTML = "Owned: " + localStorage.getItem(i + "_" + 0);
+        }
+        
     }
+
+    for (let i = 0; i < artifacts.length; i++){
+        artifactsOwned.set(artifacts[i], localStorage.getItem(artifacts[i]));
+    }
+
+}
+
+function reset(){
+
+    gooses = 0;
+    multiplier = 1;
+    items = origItems.slice(); //slice to create different array, not reference to origItems
+
+    for (let i = 0; i < items.length; i++){
+        //Update html text
+        htmlItems[i][1].innerHTML = "Price: " + items[i][1];
+        htmlItems[i][2].innerHTML = "Owned: " + items[i][0];
+    }
+
+    console.log("RESET");
 
 }
 
@@ -61,7 +108,6 @@ chipmunk.addEventListener('click', function(){
         else gooses *= 0.5;
         console.log(gooses);
     }
-    gooses += 1000000;
 });
 
 //Prestige overlay activate
@@ -88,11 +134,90 @@ prestigeYes.addEventListener('click', function(){
     prestigeOverlay.style.visibility = "hidden";
 })
 
+//Settings//
+
+const settingsOverlay = document.getElementById("settingsOverlay");
+
+//Open settings
+const settingsButton = document.getElementById("settingsButton");
+settingsButton.addEventListener('click', function(){
+    settingsOverlay.style.visibility = "visible";
+});
+
+//Close settings
+const settingsClose = document.getElementById("settingsClose");
+settingsClose.addEventListener('click', function(){
+    settingsOverlay.style.visibility = "hidden";
+});
+
+//Reset Button
+const resetOverlay = document.getElementById("resetOverlay");
+const resetButton = document.getElementById("resetButton");
+resetButton.addEventListener('click', function(){
+    resetOverlay.style.visibility = "visible";
+});
+
+//Textbox
+const twoFactor = document.getElementById("twoFactor");
+
+const resetClose = document.getElementById("resetClose");
+resetClose.addEventListener('click', function(){
+    resetOverlay.style.visibility = "hidden";
+});
+
+//Reset Error Box
+var opacity = 100;
+const resetError = document.getElementById("resetError");
+const resetConfirm = document.getElementById("resetConfirm");
+resetConfirm.addEventListener('click', function(){
+    //SUCCESSFUL
+    if (twoFactor.value === "RESET"){
+        localStorage.clear();
+        reset();
+        resetOverlay.style.visibility = "hidden";
+        settingsOverlay.style.visibility = "hidden";
+    }
+    //If First Click
+    else if (opacity == 100){
+        opacity = 99;
+        resetError.style.opacity = "100%";
+        setTimeout(function(){
+            var intervalID = setInterval(function(){
+                opacity--;
+                resetError.style.opacity = opacity + "%";
+                if (opacity == 0){
+                    clearInterval(intervalID);
+                    opacity = 100;
+                }
+            }, 15);
+        }, 1000);
+    }
+    //Already Clicked, Mid Fading
+    else{
+        resetError.style.opacity = "99%";
+        opacity = 99;
+    }
+    twoFactor.value = "";
+});
+
+//Settings END//
+
+//////////
+//NEWS////
+//////////
+
+const news = document.getElementById(("news"));
+var newsBank = ["After geese took over the world in 2069, geese have now become the main currency.", "Scientists say it may start raining geese!", "News: 420 geese have moved into the University of Waterloo today."]
+
+setInterval(function(){
+    news.innerHTML = newsBank[Math.floor(newsBank.length * Math.random())];
+}, 15000);
+
 //Click on goose event
 mainGoose.addEventListener('click', function(){
     gooses += 1 * multiplier;
     gooseThisSecond += 1 * multiplier;
-    gooseCount.innerHTML = gooses + " Geese";
+    gooseCount.innerHTML = Math.round(gooses) + " Geese";
 });
 
 //ITEMS//
@@ -110,6 +235,26 @@ for (let i = 0; i < htmlItems.length; i++){
     });
 }
 
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//ARTIFACTS
+//NOTE: Artifacts are too specialized and randomized to be condensed into a class
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+const fertilizer = document.getElementById("fertilizer");
+fertilizer.addEventListener('click', function(){
+    console.log("clicked");
+    if (gooses >= 1000 && artifactsOwned.get("fertilizer") != "true"){
+        gooses -= 1000;
+        artifactsOwned.set("fertilizer", "true");
+        items[0][2] *= 2;
+    }
+});
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//ARTIFACTS END
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
 //This interval does 2 things:
 //Add geese from items
 //Count geese per second
@@ -123,10 +268,11 @@ setInterval(function(){
     goosePerSecond.innerHTML = "Geese per second: " + gooseThisSecond;
     gooseThisSecond = 0;
 
-    gooseCount.innerHTML = gooses + " Geese";
+    gooseCount.innerHTML = Math.round(gooses) + " Geese";
     
 }, 1000);
 
+//Autosave Feature
 setInterval(function(){
 
     localStorage.setItem("gooses", gooses);
@@ -136,6 +282,10 @@ setInterval(function(){
         for (let j = 0; j < 3; j++){
             localStorage.setItem(i + "_" + j, items[i][j]);
         }
+    }
+
+    for (let [key, value] of artifactsOwned){
+        localStorage.setItem(key, value);
     }
 
     console.log("Saved!");
